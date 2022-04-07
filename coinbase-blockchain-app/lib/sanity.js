ce7c85f04f533815cd84766dba11e8476e926fbc
Kinga Ozimek <kingaozime@gmail.com>
2022-04-08 02:23:23 +0300

    sanity-setup

---
+++ coinbase-blockchain-app/lib/sanity.js
@@ -0,0 +1,10 @@
+import sanityClient from '@sanity/client'
+
+export const client = sanityClient( {
+  projectId: 'r0sxj5h0',
+  dataset: 'production',
+  apiVersion: '2021-03-25',
+  token:
+  'skypZ0Inn4cjB7EeLGGR7tT7dKqCFATWhFQc1Qb3Km4Cw6g6oiZlQro5SqtO9tOFxdLW7gFpwDy2c83DBvThKyvPTavSQRjujYf8cpUmXVbTg6DqQjGseQJZU9aDdPIx5Vuu39lq3zh58R3Mt7CuPmGvJcpKz8nE3Dsxv4lXANNg2Vv4k6mm',
+  useCdn: false,
+})
